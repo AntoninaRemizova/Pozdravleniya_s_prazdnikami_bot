5528eeb9b4ab6061b3ce35d7ba7cdf3dcaf49f51
Антонина Ремизова <tonya_remizova@mail.ru>
2024-06-19 21:39:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Newtonsoft.Json;
+using Aspose.Cells;
+using System.IO;
+using File = System.IO.File;
+using System.Xml.Linq;
+using Telegram.Bot.Polling;
+
+namespace Pozdravleniya_s_prazdnikami_bot
+{
+    public class JsonFile
+    {
+        public static Dictionary<long, List<Host.ExcelData>> chatData;
+        public static string ChatDataFile = "ChatData.json";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host bot = new Host("7004222316:AAGmR-srArzAFigMTHawje756qnAyb-E6JM");
+            if (File.Exists(JsonFile.ChatDataFile))
+            {
+                var json = File.ReadAllText(JsonFile.ChatDataFile);
+                JsonFile.chatData = JsonConvert.DeserializeObject<Dictionary<long, List<Host.ExcelData>>>(json);
+            }
+            else
+            {
+                JsonFile.chatData = new Dictionary<long, List<Host.ExcelData>>();
+            }
+            bot.Start();
+            Console.ReadLine();
+            var stateJson = JsonConvert.SerializeObject(JsonFile.chatData);
+            File.WriteAllText(JsonFile.ChatDataFile, stateJson);
+        }
+    }
+}
